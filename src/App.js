1fe90f80094f19df5cab5599b2f1f659fd4c08ec
TURKCELL\EXT02D47957 <email@turkcell.com.tr>
2022-11-27 14:30:39 +0300

    creating react project template

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> We made our first react application</p>
-      </header>
+        <div>
+          <div>
+            <div>
+              Your Todo's
+            </div>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Assigned</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope='row'>1</th>
+                    <td >Feed dog</td>
+                    <td >Mücahit</td>
+                  </tr>
+                  <tr>
+                    <th scope='row'>2</th>
+                    <td >Get haircut</td>
+                    <td >Mücahit</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
